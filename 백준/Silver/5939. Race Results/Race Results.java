f9f08b8ce63765cbc8590a2c4a27dcb3efbdfd8d
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-06-13 22:04:38 +0900

    [Silver V] Title: Race Results, Time: 348 ms, Memory: 18608 KB -BaekjoonHub

---
+++ 백준/Silver/5939. Race Results/Race Results.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][3];
+
+        for(int i = 0 ; i < n ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0]){
+                    if(o1[1]==o2[1]){
+                        return o1[2] - o2[2];
+                    }
+                    return o1[1]-o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.println(arr[i][0]+" "+arr[i][1]+" "+arr[i][2]);
+        }
+
+
+
+
+    }
+}
