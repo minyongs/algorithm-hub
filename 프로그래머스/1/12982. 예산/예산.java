1a3a5c47369148fa651722baa9515ecf1b445dd4
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-06-17 09:30:48 +0900

    [level 1] Title: 예산, Time: 0.66 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int ans = 0;
+        int sum = 0;
+        for(int i =0; i <d.length;i++){
+            sum+=d[i];
+            if(sum>budget){
+                ans = i;
+                break;
+            }
+        }
+        
+        if(sum<=budget){
+            ans = d.length;
+        }
+        
+        return ans; 
+        
+        
+        
+        
+        
+    }
+}
