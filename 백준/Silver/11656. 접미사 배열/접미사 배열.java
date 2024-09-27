afbcbfef8a909f1192149a422de6803441c2363a
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-09-27 19:22:02 +0900

    [Silver IV] Title: 접미사 배열, Time: 164 ms, Memory: 16020 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -1,24 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//덩치
-//실버5
 public class Main {
-    public static void main(String[] args) throws Exception{
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s = br.readLine();
         String[] arr = new String[s.length()];
-        for(int i=0;i<s.length();i++){
-            String sub = s.substring(i,s.length());
-            arr[i] = sub;
+        for(int i = 0 ; i < s.length() ; i++){
+            arr[i] = s.substring(i,s.length()); // 배열에 저장
         }
 
-        Arrays.sort(arr);
-        
-        for(String ans : arr){
+        Arrays.sort(arr); // 사전순 정렬
+
+        for(String ans : arr){// 출력
             System.out.println(ans);
         }
 
