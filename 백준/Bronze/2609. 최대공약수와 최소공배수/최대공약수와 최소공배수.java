6b76eb3cbe1a450f25b24afed28db725b6fa7468
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-09-29 19:17:57 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -5,29 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a =Integer.parseInt(st.nextToken());
-        int b= Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         System.out.println(gcd(a,b));
         System.out.println(lcm(a,b));
 
-
     }
 
-    public static int gcd(int a,int b){
-        while(b != 0){
-            int temp = b ;
-            b = a % b;//6
-            a = temp;//18
+
+
+    public static int gcd(int a, int b) { // 나머지가 0 이 될때까지 나누는 유클리드 호제법인가 라네요 ..
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
         return a;
     }
 
-    public static int lcm(int a , int b){
-        return a*b/gcd(a,b);
+    private static int lcm(int a, int b){
+        return  (a * b)/gcd(a,b);
     }
-
-
 }
