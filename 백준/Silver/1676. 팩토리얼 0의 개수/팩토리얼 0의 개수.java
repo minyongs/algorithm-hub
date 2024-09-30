996aa639de1acc6d8ed9c0b4680af353f40ca78a
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-09-30 18:51:02 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class Main {
 
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = in.nextInt();
+
+        int num = Integer.parseInt(br.readLine());
         int count = 0;
+        // 0의 개수는 5의 배수가 결정함! 
 
         while (num >= 5) {
-            count += num / 5;
-            num /= 5;
+            count += num / 5; //CNT 증가
+            num /= 5; // 나눠진 숫자도 5가 들어가 있는지 확인 필요
         }
         System.out.println(count);
     }
 }
 
 
-    
+
