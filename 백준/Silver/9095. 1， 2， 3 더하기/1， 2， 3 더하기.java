5ab3cc07e57e3cb7107f03d3626b8c94296c6695
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-10-08 20:43:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,31 +1,26 @@
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-//1 2 3 더하기[실버3]
 public class Main {
-    public static void main(String[] args) throws Exception{
+    private static int[] dp = new int[12];
+    //시간 복잡도 O(T)
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dp = new int[12];
-
-        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        for(int i = 4; i<=11; i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        //DP[N] = DP[n-3] + DP[n-2] + DP[n-1] 의 규칙성을 가짐 .. 즉 앞에 세개 더한게 다음것
+        for(int i = 4 ; i <= 11; i ++){ // 11까지의 DP 배열 초기화
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
         }
 
-        int n =Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < n; i++){
-            int index = Integer.parseInt(br.readLine());
-            System.out.println(dp[index]);
-
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < T; i++){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
         }
-
-
-
-
-
     }
 }
