e00e2bd6461beb4f2fa9f2fe62349563b9d34676
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-10-22 23:30:22 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,25 +1,33 @@
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        String s = String.valueOf(n);
+        int target = make(n,s);
         int ans = 0;
-        int digit = 1; // 더할 자릿수
-        int range = 10; //검사중인 범위
-        for(int i = 1; i <= n; i++){
-            if(i % range == 0){ //range 로 나누어떨어지면 자릿수가 바뀌었단 의미
-                digit ++; // 더해주는 자릿수도 바꿔줘야함
-                range *= 10; //10을 곱한 자릿수로 !
-            }
-            ans += digit; //계속더해
+        for(int i = s.length() ; i >= 1; i--){
+            ans += (n - target + 1) * i;
+            n = target - 1;
+            target /= 10;
         }
 
         System.out.println(ans);
+    }
 
+    //8243
+
+    private static int make(int n,String s){
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        while (sb.length() != s.length()){
+            sb.append(0);
+        }
+        return Integer.parseInt(sb.toString());
     }
 }
