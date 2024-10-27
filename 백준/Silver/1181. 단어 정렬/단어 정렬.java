81b48b8d0dbbcde9bb8d3c40a31d55dd18c1fcc3
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-10-27 22:40:13 +0900

    [Silver V] Title: 단어 정렬, Time: 1568 ms, Memory: 24732 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+
+        List<String> ans = new ArrayList<>();
+        for(String s : arr){
+            if(!ans.contains(s)){
+                ans.add(s);
+            }
+        }
+        for(String s : ans){
+            System.out.println(s);
+        }
+    }
+}
