abe157ca42c32d2c7eca935f2ae2a57c4ff3a2e4
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-11-06 00:27:32 +0900

    [D3] Title: 두가지 빵의 딜레마, Time: 118 ms, Memory: 18,736 KB -BaekjoonHub

---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/README.md
@@ -0,0 +1,15 @@
+# [D3] 두가지 빵의 딜레마 - 5162 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWTaTDua3OoDFAVT) 
+
+### 성능 요약
+
+메모리: 18,736 KB, 시간: 118 ms, 코드길이: 768 Bytes
+
+### 제출 일자
+
+2024-11-06 00:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/두가지 빵의 딜레마.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine()); //TC 개수
+
+        for(int i = 1 ; i <= T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int min = Math.min(a,b);
+
+            int ans = c / min;
+
+            System.out.println("#"+i+" "+ans);
+        }
+    }
+}
