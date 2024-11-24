5a2787177e3c6ccfc611bdc48faab07ae1b3dc98
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-11-24 21:36:55 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,37 +1,44 @@
+
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int w, h;
-    public static int[][] map;
-    public static boolean[][] visited;
+    static int[][] map;
+    static boolean[][] visited;
+    static int w, h;
+    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
 
-            if (w == 0 && h == 0) {
+        while (true) {
+            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(stringTokenizer.nextToken()); // 클래스 변수에 할당
+            h = Integer.parseInt(stringTokenizer.nextToken()); // 클래스 변수에 할당
+
+            if (h == 0 && w == 0) {
                 break;
             }
 
             map = new int[h][w];
             visited = new boolean[h][w];
 
+            // 지도 정보 입력
             for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
+                stringTokenizer = new StringTokenizer(br.readLine());
                 for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
                 }
             }
 
-            int cnt = 0;
+            int cnt = 0; // 섬의 개수
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
+                    if (!visited[i][j] && map[i][j] == 1) {
                         dfs(i, j);
                         cnt++;
                     }
@@ -41,16 +48,16 @@ public class Main {
         }
     }
 
-    public static void dfs(int x, int y) {
-        int[] dx = {0, 0, -1, 1, -1, 1, -1, 1};
-        int[] dy = {-1, 1, 0, 0, -1, 1, 1, -1};
 
+    private static void dfs(int x, int y) {
         visited[x][y] = true;
+
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx > -1 && nx < h && ny > -1 && ny < w && map[nx][ny] == 1 && !visited[nx][ny]) {
+
+            if (nx >= 0 && ny >= 0 && nx < h && ny < w && !visited[nx][ny] && map[nx][ny] == 1) {
                 dfs(nx, ny);
             }
         }
