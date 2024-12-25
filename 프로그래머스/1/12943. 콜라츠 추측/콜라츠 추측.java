5c55ff96276b74eea7019c12817c27e54cf07c43
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2024-12-25 19:54:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(long num) {
+        if(num == 1){
+            return 0;
+        }
+        int cnt = 0 ;
+        while(num!=1){
+            if(num % 2 ==0){
+                num /= 2;
+                cnt ++;
+            }else {
+                num = (num*3)+1;
+                cnt ++;
+            }
+            
+            
+            if(cnt>500){
+                return -1;
+                
+            }
+        }
+       
+            return cnt;
+    }
+}
