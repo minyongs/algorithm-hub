ce25e92e603246af5a502e5742a01a261bcd4fc3
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2025-01-06 16:33:40 +0900

    [Silver IV] Title: 카드2, Time: 268 ms, Memory: 49012 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i <n ; i ++){
+            queue.add(i+1);
+
+        }
+        
+        while (queue.size() >1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+
+
+
+
+
+
+    }
+}
