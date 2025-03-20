661c19979a78a1af8f4828d6f33f8c3fdae8b76b
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2025-03-20 13:48:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.41 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 25.28 ms
+메모리: 112 MB, 시간: 51.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 13일 02:22:45
+2025년 03월 20일 13:48:08
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,28 +2,18 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-     List<Integer> arrList = new ArrayList<>();
-        
-        arrList.add(arr[0]); //인덱스 0 미리 넣어두기
-        
-        for(int i = 1 ; i <arr.length; i++){
-            
-            if(arr[i] == arr[i-1]){//index i 가 i-1 과 같으면 pass
-                continue;
-            }else{
-                arrList.add(arr[i]); //다르면 arrList에 추가
+    
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            if(stack.peek() != arr[i]){
+                stack.push(arr[i]);
             }
- 
         }
-        //arrList 를 int 배열로 변환
-        int[] answer = new int[arrList.size()];
-        for(int i = 0; i < arrList.size(); i++){
-            answer[i] = arrList.get(i);
-        }
-       
-            
-        
-
-        return answer;
+        int[] ans = new int[stack.size()];
+        for(int i = stack.size() - 1; i >= 0 ; i--){
+            ans[i] = stack.pop();
+        }      
+        return ans;
     }
 }
