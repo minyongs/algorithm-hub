6040e4a9cf9c2a02b0d0560537b2051726763a22
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2025-04-24 15:03:29 +0900

    [Silver IV] Title: 학생 번호, Time: 120 ms, Memory: 16596 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.java
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -1,38 +1,42 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> set = new HashSet<>();
         int n = Integer.parseInt(br.readLine());
         String[] arr = new String[n];
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0 ; i < n; i++){
             arr[i] = br.readLine();
         }
 
         int idx = arr[0].length() - 1;
-        int ans = arr[0].length();
-        Set<String> set = new HashSet<>();
-
-        while (idx >= 0) {
-            for (int i = 0; i < n; i++) {
-                String s = arr[i].substring(idx, arr[0].length());
+        int ans = 0;
+        while(idx>=0){
+            String s = "";
+            for(int i = 0 ; i < n; i++){
+                s = arr[i].substring(idx,arr[0].length());
                 set.add(s);
             }
-            if (set.size() == n) {
-                ans = arr[0].length() - idx;
+            if(set.size() == n){
+                ans = s.length();
                 break;
-            } else {
-                idx--;
+            }else{
+                idx --;
                 set.clear();
             }
         }
-
         System.out.println(ans);
+
+
     }
 }
