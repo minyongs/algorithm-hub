249ac411cc827d304ab6c2cbff615739c46035dc
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2025-06-18 21:09:59 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-    String strNum = Integer.toString(n); // 정수를 문자열로 변환
-        int[] arr = new int[strNum.length()]; // strNum 의 길이만큼의 int 형 배열 생성
-        for(int i = 0; i < strNum.length();i ++){
-            arr[i]  = strNum.charAt(i) - '0'; //charAt을 사용하여 인덱스 위치를 지정하고, - '0' int 로 변환
-            answer += arr[i];        }
+        String s = String.valueOf(n);
+        char[] arr = s.toCharArray();
         
-        // 오류해결 : toString() 으로 변환 후 생성된 문자열의 길이는 strNum.length() 처럼 괄호가 꼭 붙어있어야 한다!
-       
-        return answer;
+        int sum = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            sum += arr[i] - '0';
+        }
+        
+        return sum;
     }
 }
