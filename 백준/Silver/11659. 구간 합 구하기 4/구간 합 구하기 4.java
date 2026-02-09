a4c6de6d1058b396a162a539e9107474c98aec93
Minhyung Song <91649302+minyongs@users.noreply.github.com>
2026-02-09 22:09:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1164 ms, Memory: 63408 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,35 +1,46 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        //처음에 주어진 배열이 변하지 않고 , 엄청나게 많은 횟수가 있을땐 항상 구간합 고려해보자 !
-        //배열이 변하지 않으니 합 배열을 만들자
-        //받는 데이터가 많을땐 bufferReader 사용 추천
-
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        //들어오자마자 바로바로 합배열 만들기
-        long [] S= new long[n+1]; //인덱스를 1부터 쓸것임
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        stringTokenizer = new StringTokenizer(br.readLine());
-
-        for(int i =1; i<=n ; i++){
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken()); // 합배열 공식 -> s[i] = s[i-1] + A[i]
-        }
-        for(int q = 0 ; q<m;q ++){
-            stringTokenizer = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j]-S[i-1]);
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0 ; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[] prefix = new int[N];
 
-    }}
+        prefix[0] = arr[0];
+
+        for(int i = 1 ; i < N; i++){
+            prefix[i] = prefix[i - 1] + arr[i]; // 구간합
+        }
+
+
+        // M번 순회
+        for(int i = 0 ; i < M; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int ans = 0;
+            int k = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            if(k != 1){
+                ans = prefix[j - 1] - prefix[k - 2];
+            }else{
+                ans = prefix[j-1];
+            }
+
+            System.out.println(ans);
+        }
+
+    }
+}
